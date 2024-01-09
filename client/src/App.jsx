245b81156293928d00b827435454d2046279a084
harmanbrar7 <harmanbrar7@hotmail.com>
2024-01-09 00:58:07 -0800

    Updated App.jsx with Context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,23 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage.jsx';
 import SignIn from './pages/SignIn.jsx';
 import Home from './pages/Home.jsx';
+import { CannibasProvider } from './context/context.jsx';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/home" element={<Home />} />
-        {/* Add other routes as needed */}
-      </Routes>
+
+      <CannibasProvider>
+
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/home" element={<Home />} />
+          {/* Add other routes as needed */}
+        </Routes>
+
+      </CannibasProvider>
+      
     </BrowserRouter>
   );
 };
